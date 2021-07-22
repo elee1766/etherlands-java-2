04599eee3f6525d81e42b1431894aff6f162ab60
father <86447830+blowl@users.noreply.github.com>
2021-07-21 18:08:02 -0700

    Add files via upload

--- src/main/java/etherlandscore/etherlandscore/slashcommands/FriendCommand.java
+++ src/main/java/etherlandscore/etherlandscore/slashcommands/FriendCommand.java
@@ -4,16 +4,20 @@ import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.StringArgument;
 import etherlandscore.etherlandscore.fibers.Channels;
 import etherlandscore.etherlandscore.services.ListenerClient;
+import etherlandscore.etherlandscore.services.MasterService;
 import etherlandscore.etherlandscore.state.Gamer;
+import etherlandscore.etherlandscore.state.LocaleStrings;
 import org.jetlang.fibers.Fiber;
 
 public class FriendCommand extends ListenerClient {
     private final Fiber fiber;
     private final Channels channels;
+    private final LocaleStrings locales = new LocaleStrings();
     public FriendCommand(Channels channels, Fiber fiber) {
         super(channels, fiber);
         this.fiber = fiber;
         this.channels = channels;
+
     }
 
     public void register(){
@@ -30,9 +34,9 @@ public class FriendCommand extends ListenerClient {
                     if(!gamer.getFriends().contains(newFriend)){
                         gamer.addFriend(this.channels, newFriend);
                     }else{
-                        sender.sendMessage((newFriend + " was already on your friends list."));
+                        sender.sendMessage(locales.getFriends().get("fail"));
                     }
-                    sender.sendMessage(newFriend + " was added to friends list.");
+                    sender.sendMessage(locales.getFriends().get("success"));
                 }));
         FriendCommand.register();
     }

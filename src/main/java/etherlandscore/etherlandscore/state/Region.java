c248437c1059e55e77d0e1513075f4973c34db8b
elee1766 <edward9.lee@gmail.com>
2021-07-24 17:32:24 -0500

    lots of simplifications

--- src/main/java/etherlandscore/etherlandscore/state/Region.java
+++ src/main/java/etherlandscore/etherlandscore/state/Region.java
@@ -5,18 +5,20 @@ import etherlandscore.etherlandscore.enums.FlagValue;
 import etherlandscore.etherlandscore.fibers.Channels;
 import etherlandscore.etherlandscore.fibers.MasterCommand;
 import etherlandscore.etherlandscore.fibers.Message;
+import etherlandscore.etherlandscore.util.Map2;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+
+import static etherlandscore.etherlandscore.services.MasterService.state;
 
 public class Region extends StateHolder implements Comparable<Region> {
   private final Set<Integer> plotIds;
   private final String team;
   private final String name;
   private final boolean isDefault;
-  private final Map<UUID, Map<AccessFlags, FlagValue>> gamerFlags = new HashMap<>();
+  private final Map2<String, AccessFlags, FlagValue> groupPermissionMap = new Map2<>();
+  private final Map2<UUID, AccessFlags, FlagValue> gamerPermissionMap = new Map2<>();
   private Integer priority;
 
   Region(Team team, String name, Set<Integer> plotIds, Integer priority, boolean isDefault) {
@@ -25,6 +27,17 @@ public class Region extends StateHolder implements Comparable<Region> {
     this.name = name;
     this.isDefault = isDefault;
     this.priority = priority;
+  }
+
+  public void setGroupPermission(Channels channels, Group group, AccessFlags flag, FlagValue value){
+    channels.master_command.publish(
+        new Message<>(MasterCommand.region_set_group_permission,this,group,flag,value)
+    );
+  }
+  public void setGamerPermission(Channels channels, Gamer gamer, AccessFlags flag, FlagValue value){
+    channels.master_command.publish(
+        new Message<>(MasterCommand.region_set_gamer_permission,this,gamer,flag,value)
+    );
   }
 
   public void addPlot(Channels channels, Plot plot) {
@@ -54,25 +67,30 @@ public class Region extends StateHolder implements Comparable<Region> {
     if(newPriority > 100) this.priority = 0;
   }
 
-  @Override
-  public int compareTo(Region r) {
-    return getPriority().compareTo(r.getPriority());
-  }
 
   public FlagValue checkFlags(AccessFlags flag, Gamer gamer) {
-    var flagMap = gamerFlags.getOrDefault(gamer.getUuid(), new HashMap<>());
-    return flagMap.getOrDefault(flag, FlagValue.NONE);
+    return gamerPermissionMap.getOrDefault(gamer.getUuid(),flag,FlagValue.NONE);
+  }
+
+  public FlagValue checkFlags(AccessFlags flag, Group group) {
+    return groupPermissionMap.getOrDefault(group.getName(),flag,FlagValue.NONE);
   }
 
   public boolean isDefault() {
     return isDefault;
   }
 
-  public String getTeam() {
-    return team;
+
+  public Team getTeam() {
+    return state().getTeam(getName());
   }
 
   public String getName() {
     return name;
+  }
+
+  @Override
+  public int compareTo(Region r) {
+    return getPriority().compareTo(r.getPriority());
   }
 }

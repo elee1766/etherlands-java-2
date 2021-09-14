e46e741814799c865fb1bd5e6645bd08b482e11d
blowl <bowldonations@gmail.com>
2021-09-13 19:10:47 -0700

    switched to jedis

---
+++ src/main/java/etherlandscore/etherlandscore/singleton/RedisGetter.java
@@ -0,0 +1,44 @@
+package etherlandscore.etherlandscore.singleton;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class RedisGetter {
+  public static JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost:6379");
+
+  public static String getPlotX(String key){
+    String output = "";
+    try (redis.clients.jedis.Jedis jedis = pool.getResource()) {
+      String input = ("plot:"+key+":x");
+      output = jedis.get(input);
+    }
+    pool.close();
+    return output;
+  }
+
+  public static String getPlotZ(String key){
+    String output = "";
+    try (redis.clients.jedis.Jedis jedis = pool.getResource()) {
+      String input = ("plot:"+key+":z");
+      output = jedis.get(input);
+    }
+    pool.close();
+    return output;
+  }
+
+  public static Set<String> getDistrictOfPlot(String key){
+    Double minmax = Double.parseDouble(key);
+    Set<String> districts;
+    try (redis.clients.jedis.Jedis jedis = pool.getResource()) {
+      districts = jedis.zrangeByScore("districtZplot", minmax, minmax);
+    }
+    pool.close();
+    return districts;
+  }
+
+}
--- src/main/java/etherlandscore/etherlandscore/slashcommands/PlotCommand.java
+++ src/main/java/etherlandscore/etherlandscore/slashcommands/PlotCommand.java
@@ -2,30 +2,28 @@ package etherlandscore.etherlandscore.slashcommands;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.IntegerArgument;
+import dev.jorel.commandapi.arguments.StringArgument;
 import etherlandscore.etherlandscore.fibers.Channels;
 import etherlandscore.etherlandscore.fibers.ChatTarget;
-import etherlandscore.etherlandscore.fibers.MasterCommand;
 import etherlandscore.etherlandscore.fibers.Message;
-import etherlandscore.etherlandscore.persistance.Redis.RedisReader;
 import etherlandscore.etherlandscore.services.ListenerClient;
+import etherlandscore.etherlandscore.singleton.RedisGetter;
 import etherlandscore.etherlandscore.state.read.Plot;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetlang.fibers.Fiber;
 
-import static etherlandscore.etherlandscore.services.MasterService.state;
+import java.util.Set;
 
 public class PlotCommand extends ListenerClient {
   private final Fiber fiber;
   private final Channels channels;
-  private final RedisReader redis = null;
 
   public PlotCommand(Channels channels, Fiber fiber) {
     super(channels, fiber);
     this.fiber = fiber;
     this.channels = channels;
-    //this.redis = new RedisReader();
     register();
   }
 
@@ -50,15 +48,17 @@ public class PlotCommand extends ListenerClient {
     CommandAPICommand reddisID =
         new CommandAPICommand("plotRed")
             .withPermission("etherlands.public")
+            .withArguments(new StringArgument("PlotID"))
             .executesPlayer(this::idRed);
     reddisID.register();
     ChunkCommandidLocal.register();
   }
 
   void idRed(Player player, Object[] args) {
-    String x = redis.getPlotX((String) args[0]).get(0);
-    String z = redis.getPlotZ((String) args[0]).get(0);
-    String district = redis.districtOfPlot((String) args[0]);
+    String idString = (String) args[0];
+    String x = RedisGetter.getPlotX(idString);
+    String z = RedisGetter.getPlotZ(idString);
+    Set<String> district = RedisGetter.getDistrictOfPlot(idString);
     player.sendMessage("Plot coords: " + x + ", " + z + " district: " + district);
   }
 
